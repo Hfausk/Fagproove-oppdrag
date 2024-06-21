566b53d94172bfd916391cf6036dfd3f267a701f
Herman <89454551+Hfausk@users.noreply.github.com>
2024-06-21 11:29:14 +0200

    Changed schema to have relations to tables

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,21 +1,46 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text, timestamp, primaryKey } from 'drizzle-orm/pg-core';
 
 export const students = pgTable('students', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-});
-export const books = pgTable('books', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
 });
 
-export const lending = pgTable('lending', {
-  id: serial('id').primaryKey(),
-  bookId: serial('id').notNull().references(() => books.id),
-  studentId: serial('id').notNull().references(() => students.id),
-  lentAt: timestamp('created_at').notNull().defaultNow(),
-  deliverdAt: timestamp('updated_at'),
+export const studentsRelation = relations(students, ({ many }) => ({
+    lending: many(lending),
+}));
+
+export const books = pgTable('books', {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
 });
+
+export const booksRelation = relations(books, ({ many }) => ({
+    lending: many(lending),
+
+}));
+
+export const lending = pgTable('lending', {
+    bookId: serial('id').notNull().references(() => books.id),
+    studentId: serial('id').notNull().references(() => students.id),
+    lentAt: timestamp('created_at').notNull().defaultNow(),
+    deliverdAt: timestamp('updated_at'),
+}, (table) => {
+    return {
+        pk: primaryKey({ columns: [table.bookId, table.studentId, table.lentAt] })
+    }
+});
+
+export const lendingRelation = relations(lending, ({ one }) => ({
+    book: one(books, {
+        fields: [lending.bookId],
+        references: [books.id]
+    }),
+    student: one(students, {
+        fields: [lending.studentId],
+        references: [students.id]
+    }),
+}));
 
 export type InsertStudents = typeof students.$inferInsert;
 export type SelectStudents = typeof students.$inferSelect;
