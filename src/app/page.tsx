daf21b1750f871bc5d938197a3144c3a84dfd9c7
Herman <89454551+Hfausk@users.noreply.github.com>
2024-06-21 14:18:53 +0200

    deleted testing div on first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       StartPage
-      <div className=" border-b-2 border-white ">as</div>
     </main>
   );
 }
