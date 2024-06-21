4bbe4c674b4876dbb4a85061f5afe4002962fff8
Herman <89454551+Hfausk@users.noreply.github.com>
2024-06-21 11:30:44 +0200

    Changed earlier queries to better match what is needed

--- src/database/crud/fetch.ts
+++ src/database/crud/fetch.ts
@@ -1,25 +1,21 @@
 "use server"
 
 import { unstable_noStore as noStore } from 'next/cache';
-
 import { db } from '../db'
-import { lending } from '../schema';
-import { eq } from 'drizzle-orm';
 
 
 
-export async function fetchAllStudents() {
+
+export async function getAllStudents() {
     noStore() // fix for the next cache issue
 
     const students = await db.query.students.findMany({
-        orderBy: (student, { asc }) => [asc(student.name)]
+        orderBy: (student, { asc }) => [asc(student.name)],
     })
-
     return students
 }
 
-
-export async function fetchAllBooks() {
+export async function getAllBooks() {
     noStore() // fix for the next cache issue
 
     const books = await db.query.books.findMany({
@@ -27,14 +23,4 @@ export async function fetchAllBooks() {
     })
 
     return books
-}
-
-
-export async function fetchLendingsByBookId(bookId: number) {
-    noStore() // fix for the next cache issue
-
-    const lendings = await db.select().from(lending)
-    .where(eq(lending.bookId, bookId))
-
-    return lendings
 }
