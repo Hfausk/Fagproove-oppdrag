42693aa65aa4c42bada711c4485bc3776880e629
Herman <89454551+Hfausk@users.noreply.github.com>
2024-06-24 13:30:15 +0200

    First commit: added data to page from getBookId()

--- src/app/books/[slug]/page.tsx
+++ src/app/books/[slug]/page.tsx
@@ -1,17 +1,17 @@
 
 import React from "react";
+import { getBookById } from "@/database/crud/getItemById";
 
 
 export default async function page({params }: {params:{slug: string}}) {
 
+    const book = await getBookById(Number(params.slug))
 
-    // const book = await getBookInfo({
-    //     bookId: params.slug
-    // })
+
 
     return (
       <main className="flex min-h-screen flex-col items-center p-24">
-        <h1>Books: {params.slug}</h1>
+        <h1>{book.id}</h1>
         
       </main>
     );
