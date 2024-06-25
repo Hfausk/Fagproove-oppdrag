03e4c85004814826696fa30a944cbd6add0b719f
Herman <89454551+Hfausk@users.noreply.github.com>
2024-06-25 09:43:43 +0200

    style changes on books table

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -14,9 +14,10 @@ export default async function page() {
   
   
     return (
-      <main className="flex min-h-screen flex-col items-center p-24">
+      <main className="flex min-h-screen flex-col items-center p-24 w-screen">
+        <div className="w-[800px]">
         <DataTable columns={columns} data={books} />
-        <pre>{JSON.stringify(books, undefined, 2)}</pre>
+        </div>
       </main>
     );
   }
