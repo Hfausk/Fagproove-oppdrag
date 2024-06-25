9047ebf652bc3a9112f1f508c16de41de92304e0
Herman <89454551+Hfausk@users.noreply.github.com>
2024-06-25 13:30:48 +0200

    Start of Assign book component on lend page

---
+++ src/components/assignBookToStudent/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+import{ useState } from "react";
+
+export default function AssignBookToStudent() {
+    const [book, setBook] = useState({
+        id: "",
+        name: "",
+    })
+    return (
+        <div>
+            <h1>Assign Book to Student</h1>
+            
+        </div>
+    )
+}
